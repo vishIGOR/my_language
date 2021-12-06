380ca98d397aae863d69d389664348c238032899
DESKTOP-3QKRIRT\асус <vish_ig2@mail.ru>
2021-12-06 21:30:40 +0700

    created lexical analyser

--- main.py
+++ main.py
@@ -1,6 +1,52 @@
-print("Hello, Nadya!")
+from sys import exit
+import re
+
+# if("-2147480" < "-2147482"):
+#     print(True)
+# else:
+#     print(False)
+
+# pattern = r'^-?[0-9]+$'
+# some_string = "0"
+# match = re.search(pattern, some_string)
+# if (match == None):
+#     print("not found")
+# else:
+#     print(match[0])
+
+test_list = list()
+
+test_row = list()
+test_row.append(1)
+test_row.append("+")
+test_row.append("-2")
+test_list.append(test_row)
 
 
-class lexical_analyzer:
-    __init__(self):
-        print("test")
+def lexical_analyzer(list_of_operands):
+    for string in list_of_operands:
+        if(len(string) < 3):
+            print(
+                "ERROR: неверное количество операндов(меньше двух) в строке :", string[0])
+            exit()
+        if(len(string) > 3):
+            print(
+                "ERROR: неверное количество операндов(больше двух) в строке :", string[0])
+            exit()
+
+        operation_pattern = r'^[/*+$-]$'
+        operation_match = re.search(operation_pattern, string[1])
+        if(operation_match == None):
+            print("ERROR: неверный первый операнд(действие) в строке :", string[0])
+            exit()
+
+        number_pattern = r'^-?[1-9][0-9]*$'
+        number_match = re.search(number_pattern, string[2])
+        if(number_match == None):
+            print("ERROR: неверный второй операнд(число) в строке :", string[0])
+            exit()
+
+
+lexical_analyzer(test_list)
+
+print("all is ok")
