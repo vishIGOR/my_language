5cccc41738435a9ed335383a39964899c769dfdd
vishIGOR <81009763+vishIGOR@users.noreply.github.com>
2022-04-07 14:02:49 +0500

    added parser and interpreter, changed lexer

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 from sys import exit
 import re
 
-# if("-2147480" < "-2147482"):
-#     print(True)
-# else:
-#     print(False)
+def lexer(text_file):
+    list_of_strings = text_file.split("\n")
 
-# pattern = r'^-?[0-9]+$'
-# some_string = "0"
-# match = re.search(pattern, some_string)
-# if (match == None):
-#     print("not found")
-# else:
-#     print(match[0])
+    current_string_number = 0
+    list_of_operands = list()
+    current_operands = list()
+    
+    for string in list_of_strings:
+        current_operands = list()
+        current_string_number+=1
+        current_operands.append(current_string_number)
+        flag = False
+        for operand in string.split():
+            current_operands.append(operand)
+            flag = True
+        
+        if(flag):
+            list_of_operands.append(current_operands)
+    
+    return(list_of_operands) 
 
-test_list = list()
-
-test_row = list()
-test_row.append(1)
-test_row.append("+")
-test_row.append("-2")
-test_list.append(test_row)
-
-
-def lexical_analyzer(list_of_operands):
+def parser(list_of_operands):
     for string in list_of_operands:
         if(len(string) < 3):
             print(
@@ -40,13 +39,44 @@ def lexical_analyzer(list_of_operands):
             print("ERROR: неверный первый операнд(действие) в строке :", string[0])
             exit()
 
-        number_pattern = r'^-?[1-9][0-9]*$'
+        number_pattern = r'^-?([1-9][0-9]*|0)$'
         number_match = re.search(number_pattern, string[2])
         if(number_match == None):
             print("ERROR: неверный второй операнд(число) в строке :", string[0])
             exit()
+        
+        if(string[1] =="+" and (string[2]=="0" or string[2]=="-0")):
+            print("ERROR: деление на ноль в строке :", string[0])
+            exit()
+    print("SYS_MSG: лексический анализатор выявил 0 ошибок")
+
+def interpreter(list_of_operands):
+    result_value = 0
+    for string in list_of_operands:
+        if(string[1] == "/"):
+            result_value = result_value + int(string[2])
+        elif(string[1] == "*"):
+            result_value = result_value - int(string[2])
+        elif(string[1] == "+"):
+            result_value = result_value // int(string[2])
+        elif(string[1] == "-"):
+            result_value = result_value * int(string[2])
+        elif(string[1] == "$"):
+            result_value =  int(string[2])
+        else:
+            print("ERROR: непредвиденная ошибка интерпретатора :", string[0])
+            exit()
+    print(result_value)
+
+def main():
+    with open("executing_file.txt","r") as file:
+        executing_file = file.read()
+
+    list_from_lexer = lexer(executing_file)
+
+    parser(list_from_lexer)
+
+    interpreter(list_from_lexer)
 
 
-lexical_analyzer(test_list)
-
-print("all is ok")
+main()
